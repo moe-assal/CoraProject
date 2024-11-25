4342c845f94ebc64c422f5333187965d0e8200bf
Moe Asal <moritz.asal.04@gmail.com>
2024-11-25 17:33:33 +0200

    GAT fixed 2

--- models/GAT.py
+++ models/GAT.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torch_geometric.nn import GAT
+from torch_geometric.nn import GATConv
 
 
 class GATNetwork(nn.Module):
@@ -18,28 +18,28 @@ class GATNetwork(nn.Module):
         self.layers = nn.ModuleList()
 
         # input layer
-        self.layers.append(GAT(in_channels, hidden_channels, heads=num_heads, num_layers=1))
+        self.layers.append(GATConv(in_channels, hidden_channels, heads=num_heads))
         if layer_norm:
             self.layers.append(nn.LayerNorm(hidden_channels * num_heads))  # Because of multi-head, output size is num_heads * hidden_channels
         if dropout:
             self.layers.append(nn.Dropout(dropout))
         self.layers.append(activation())
 
-        # hidden layers: GATv2Conv + LayerNorm + Dropout
+        # hidden layers: GATConv + LayerNorm + Dropout
         for _ in range(num_layers - 2):
-            self.layers.append(GAT(hidden_channels * num_heads, hidden_channels, heads=num_heads, num_layers=1))
+            self.layers.append(GATConv(hidden_channels * num_heads, hidden_channels, heads=num_heads))
             if layer_norm:
                 self.layers.append(nn.LayerNorm(hidden_channels * num_heads))
             if dropout:
                 self.layers.append(nn.Dropout(dropout))
             self.layers.append(activation())
 
-        # final layer: GATv2Conv (without multiple heads)
-        self.layers.append(GAT(hidden_channels * num_heads, out_channels, heads=1, num_layers=1))
+        # final layer: without multiple heads
+        self.layers.append(GATConv(hidden_channels * num_heads, out_channels, heads=1))
 
     def forward(self, x, edge_index):
         for i in range(0, len(self.layers) - 1, 3):
-            x = self.layers[i](x, edge_index)  # apply GATv2Conv
+            x = self.layers[i](x, edge_index)  # apply GATConv
             x = self.layers[i + 1](x)  # layer norm
             x = self.layers[i + 2](x)  # dropout
             x - self.layers[i + 3](x) # activation
