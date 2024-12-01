e7a5c4a1ca20cc15d30aede00afbc747675eff03
Moe Asal <moritz.asal.04@gmail.com>
2024-12-01 17:54:29 +0200

    bugs

--- analysis/class_dist.py
+++ analysis/class_dist.py
@@ -11,9 +11,12 @@ labels = data.y
 
 # Count the occurrences of each label
 label_counts = Counter(labels.tolist())
+class_labels = ["Case_Based", "Genetic_Algorithms", "Neural_Networks", "Probabilistic_Methods",
+                "Reinforcement_Learning", "Rule_Learning", "Theory"]
 
+print(label_counts.keys())
 # Plot the class distribution
-plt.bar(label_counts.keys(), label_counts.values(), color='skyblue')
+plt.bar([class_labels[int(key)] for key in label_counts.keys()], label_counts.values(), color='skyblue')
 plt.xlabel('Class Label')
 plt.ylabel('Frequency')
 plt.title('Class Distribution in Cora Dataset')
